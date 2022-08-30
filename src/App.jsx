52c33a0029d87f3c9a54e9a1901c97f014cfb81b
Rohald van Merode <rohald_89@live.nl>
2022-08-30 15:27:20 +0200

    set up utility classes for typography and added the colors, fontsizes and fonts to the tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import Header from "./components/header"
+import Sidebar from "./components/sidebar"
+
 function App() {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Sidebar />
+        <Header />
+        <div>
+            <h1 className="heading-md">In App Heading (M)</h1>
+            <h2 className="heading-sm">In App Heading (S)</h2>
+            <p className="app-body">In App Body (S)</p>
+        </div>
+        <div className="preview">
+            <h1 className="heading-1">Preview H1</h1>
+            <h2 className="heading-2">Preview H2</h2>
+            <h3 className="heading-3">Preview H3</h3>
+            <h4 className="heading-4">Preview H4</h4>
+            <h5 className="heading-5">Preview H5</h5>
+            <h6 className="heading-6">Preview H6</h6>
+
+            <p className="p-reg">Preview Paragraph - Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </p>
+            <p className="p-bold">Preview Paragraph Bold -  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </p>
+            <p className="p-mono">Preview Paragraph Bold -  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </p>
+        </div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      "heading-md": ["0.9375rem", "1.125rem"],
+      "heading-sm": ["0.875rem", "1rem"],
+      "app-body": ["0.8125rem", "0.9375rem"],
+      "prev-heading-1": ["2rem", "2.625rem"],
+      "prev-heading-2": ["1.75rem", "2.3125rem"],
+      "prev-heading-3": ["1.5rem", "2rem"],
+      "prev-heading-4": ["1.25rem", "1.625rem"],
+      "prev-heading-5": ["1rem", "1.3125rem"],
+      "prev-heading-6": ["0.875rem", "1.125rem"],
+      "prev-body": ["0.875rem", "1.5rem"],
+    },
+    extend: {
+      colors: {
+        "grey-100": "#FFFFFF",
+        "grey-200": "#F5F5F5",
+        "grey-300": "#E4E4E4",
+        "grey-400": "#C1C4CB",
+        "grey-500": "#7C8187",
+        "grey-600": "#5A6069",
+        "grey-700": "#35393F",
+        "grey-800": "#2B2D31",
+        "grey-900": "#1D1F22",
+        "grey-1000": "#151619",
+        orange: "#E46643",
+        "orange-hover": "#F39765",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        mono: ["Roboto Mono", "monospace"],
+        serif: ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
 };
